f3f5c13eef7b4d1ef1a7cc35a0e9b333701d1451
Gui_12 <127340169+0Toco12@users.noreply.github.com>
2023-09-06 19:27:19 -0300

    .

--- guilherme/teste entrada dados/main.js
+++
@@ -1,6 +0,0 @@
-function pegarData() {
-    const timeElapsed = Date.now();
-    const today = new Date(timeElapsed);
-    const hoje = today.toLocaleDateString();
-    document.getElementById('data-cadastro').value = hoje
-}    
